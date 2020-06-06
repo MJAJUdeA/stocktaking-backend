131e342295e93fdfe0a20b658bb1e32beb0cbfdc
manaranjoc <angel.naranjo@udea.edu.co>
2020-06-06 01:26:25 -0500

    Initialize project structure

---
+++ src/main/java/com/mjaj/stocktakingbackend/StocktakingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mjaj.stocktakingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktakingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktakingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjaj/stocktakingbackend/StocktakingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjaj.stocktakingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktakingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
